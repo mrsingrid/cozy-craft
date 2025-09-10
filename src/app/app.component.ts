d3a07213f75b1e496f7f0cfa894fa423c43a23d4
Ingrid Araújo <ingridaraujo@MacBook-Air-de-Ingrid.local>
2025-09-10 12:30:20 -0300

    Add initial product-card component skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [ProductCardComponent],
 })
 export class AppComponent {
   title = 'cozy-craft';
