d6f1a7493bea705227c56198a26c1b5d82d66362
Ingrid Araújo <ingridaraujo@MacBook-Air-de-Ingrid.local>
2025-09-12 20:03:09 -0300

    Update navigation-menu-overlay to navigation-menu and adjusting structure component

---
+++ src/app/components/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, output } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-menu',
+  imports: [CommonModule],
+  templateUrl: './navigation-menu.component.html',
+  styleUrl: './navigation-menu.component.scss',
+})
+export class NavigationMenuComponent {
+  categorySelected = output<string>();
+
+  selectCategory(category: string) {
+    this.categorySelected.emit(category);
+  }
+}
