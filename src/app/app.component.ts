bc5f0ea0f6c586cae82d2a4188dcc3333214c7e6
Ingrid Araújo <ingridaraujo@MacBook-Air-de-Ingrid.local>
2025-10-14 12:30:24 -0300

    Add initial routing setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [HeaderComponent, ProductCardComponent],
+  imports: [HeaderComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'cozy-craft';
