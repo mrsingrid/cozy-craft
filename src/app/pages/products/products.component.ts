924a39a16cf7d4879757db9468686ceb1f9feeea
Ingrid Araújo <ingridaraujo@MacBook-Air-de-Ingrid.local>
2025-10-14 17:50:25 -0300

    Add products page

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+
+@Component({
+  selector: 'app-products',
+  imports: [ProductCardComponent],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss',
+})
+export class ProductsComponent {}
