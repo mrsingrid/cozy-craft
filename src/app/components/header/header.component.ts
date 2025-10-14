2a16519758922830241c287594230a48cc604655
Ingrid Araújo <ingridaraujo@MacBook-Air-de-Ingrid.local>
2025-10-14 11:45:19 -0300

    Add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { NavigationMenuComponent } from '../navigation-menu/navigation-menu.component';
+import { Component, signal } from '@angular/core';
+import { OverlayComponent } from '../overlay/overlay.component';
+import { CartComponent } from '../cart/cart.component';
+
+type OverlayType = 'menu' | 'cart' | null;
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NavigationMenuComponent,
+    CommonModule,
+    OverlayComponent,
+    CartComponent,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  currentOverlay = signal<OverlayType>(null);
+
+  openOverlay(type: OverlayType) {
+    this.currentOverlay.set(type);
+  }
+
+  closeOverlay() {
+    this.currentOverlay.set(null);
+  }
+
+  onCategorySelected(category: string) {
+    this.closeOverlay();
+    console.log(`category`, category);
+  }
+
+  onChooseProducts() {
+    this.closeOverlay();
+    console.log('redireciona para home');
+  }
+}
